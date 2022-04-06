84e4a3e8caca9352cb7cd65f1197201d8e216388
hackerrank <me@hackerrank.com>
2022-04-06 13:24:37 -0400

    added starter buttons

--- todoapp.js
+++ todoapp.js
@@ -1,8 +1,22 @@
 const item = document.querySelector("button")
+const item2 = document.getElementById("1")
 
 item.addEventListener('click',e=>{
-    const val = document.querySelector("input").value
+    const val = document.getElementById("1").value
+    const due = document.getElementById("2").value
     whatbutton = item.id
+    alert(val)
     document.getElementById("item").innerText = val;
+
+})
+item2.addEventListener('keydown',e=>{
+    if(e.code === "Enter"){
+        const val = document.getElementById("1").value
+    const due = document.getElementById("2").value
+    whatbutton = item.id
+    alert(val)
+    document.getElementById("item").innerText = val;
+    }
+
 
 })
