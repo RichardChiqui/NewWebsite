f2c66eac8c3c22772fab7689a130956f1da8885f
hackerrank <me@hackerrank.com>
2022-04-09 22:06:11 -0400

    Finalized code I think

--- todoapp.js
+++ todoapp.js
@@ -1,26 +1,47 @@
 var buttonclicked = document.querySelector("button")
-var keypressed = document.getElementById("AddItem")
-
-buttonclicked.addEventListener('click',e=>{
-
-    const ToBeAdded = document.getElementById("AddItem").value
-    const due = document.getElementById("DueDate").value
-
-    var tag = document.createElement("li");
-    var text = document.createTextNode(ToBeAdded);
-    tag.appendChild(text);
-    var element = document.getElementById("List");
-    element.appendChild(tag);
+var Item = document.getElementById("AddItem")
+var Due = document.getElementById("DueDate")
 
 
 
-})
-keypressed.addEventListener('keyup',e=>{
+Item.addEventListener('keydown',e=>{
 
     if(e.key === "Enter"){
-        buttonclicked.click()
+       AddTo(0)
+    }
+})
+Due.addEventListener('keydown',e=>{
+    if(e.key === "Enter"){
+        AddTo(0)
+    }
+})
+buttonclicked.addEventListener('click', function(){AddTo(0)})
 
+function AddTo(test){
+
+    if(Item.value===""){
+        alert("Please enter something to do")
+        return;
     }
 
+    const itemVal = Item.value
+    const due = Due.value
+    if(due == null){
+        due = ""
+    }
 
-})
+    var tagDue = document.createElement("li");
+    var textDue = document.createTextNode(due);
+
+    tagDue.appendChild(textDue);
+
+    var element = document.getElementById("ListDue");
+    element.appendChild(tagDue);
+
+    var tagItem = document.createElement("li");
+    var textItem = document.createTextNode(itemVal);
+    tagItem.appendChild(textItem);
+    var element = document.getElementById("List");
+    element.appendChild(tagItem);
+
+}
